d8f72e2415e3106e6e45e46c02cfbe37fd300ee1
Taimur <w@winterdelta.com>
2023-12-11 16:52:24 +0000

    stripe API commence

---
+++ app/api/fin/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from "next/server";
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+export default async function POST(req: NextRequest, res: NextResponse) {
+	const session = await stripe.checkout.sessions.create({
+		line_items: [
+			{
+				price: "{{PRICE_ID}}",
+				quantity: 1,
+			},
+		],
+		mode: "payment",
+		success_url: `www.winterdelta.com/?success=true`,
+		cancel_url: `www.winterdelta.com/?canceled=true`,
+		automatic_tax: { enabled: true },
+	});
+	NextResponse.redirect("303", session.url);
+}
