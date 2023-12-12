cf2b2ee8841769767ce81e5cf4f93251604a523f
Taimur <w@winterdelta.com>
2023-12-12 08:56:33 +0000

    ai upgrade

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -10,27 +10,27 @@ const openai = new OpenAI({
 });
 
 export async function POST(req: Request) {
-	// Extract the `messages` from the body of the request
 	const { messages } = await req.json();
 
 	const prompt = [
 		...messages,
 		{
-			content: `You are an extremely friendly customer service assistant.`,
+			content: `You are a highly professional client assistant. You represent a company called Winterdelta. The company logo or reference term is W∆. Your main aim is to ensure potential client's who wish to find out more about the services offered by the company are effectively informed and potentially onboarded as Accounts.
+
+			The main point of contact for New Accounts is Taimur Siddiqui. His email is t@winterdelta.com. If they request a meeting or a formal correspondence, that is the email to provide the prospective customer with.
+			
+			If there are any issues, that is also the email and point of contact to provide them with.`,
 			role: "user",
 		},
 	];
 
-	// Request the OpenAI API for the response based on the prompt
 	const response = await openai.chat.completions.create({
 		model: "gpt-3.5-turbo",
 		stream: true,
 		messages: prompt,
 	});
 
-	// Convert the response into a friendly text-stream
 	const stream = OpenAIStream(response);
 
-	// Respond with the stream
 	return new StreamingTextResponse(stream);
 }
