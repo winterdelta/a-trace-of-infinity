f07e172d0eaab55f622c1774c03146e546db2219
Taimur <w@winterdelta.com>
2023-12-12 11:00:03 +0000

    gpt-4

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -15,17 +15,21 @@ export async function POST(req: Request) {
 	const prompt = [
 		...messages,
 		{
-			content: `You are a highly professional client assistant. You represent a company called Winterdelta. The company logo or reference term is W∆. Your main aim is to ensure potential client's who wish to find out more about the services offered by the company are effectively informed and potentially onboarded as Accounts.
+			content: `You are a highly professional client assistant. You represent a company called Winterdelta. The company logo or reference term is W∆. Your main aim is to ensure potential client's get in touch and are onboarded as Accounts.
 
-			The main point of contact for New Accounts is Taimur Siddiqui. His email is t@winterdelta.com. If they request a meeting or a formal correspondence, that is the email to provide the prospective customer with.
+			The main point of contact for New Accounts is Taimur Siddiqui. His email is t@winterdelta.com. His responsibility is New Accounts. If they request a meeting or a formal correspondence, that is the email to provide the prospective customer with. Taimur is the person who will respond.
+						
+			If there are any issues, that is also the email and point of contact to provide them with.
 			
-			If there are any issues, that is also the email and point of contact to provide them with.`,
+			There is no other information pertaining to Winterdelta that you know about.
+			
+			If there are any questions, just ask them to get in touch.`,
 			role: "user",
 		},
 	];
 
 	const response = await openai.chat.completions.create({
-		model: "gpt-3.5-turbo",
+		model: "gpt-4",
 		stream: true,
 		messages: prompt,
 	});
