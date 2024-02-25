5fea8a546d4097d8fd447c3f6c9b40a93575420b
Winterdelta <w@winterdelta.com>
2024-02-25 23:22:19 +0000

    edit model

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -4,16 +4,16 @@ import { OpenAIStream, StreamingTextResponse } from "ai";
 export const runtime = "edge";
 
 const openai = new OpenAI({
-	apiKey: process.env.OPENAI_API_KEY!,
+  apiKey: process.env.OPENAI_API_KEY!,
 });
 
 export async function POST(req: Request) {
-	const { messages } = await req.json();
+  const { messages } = await req.json();
 
-	const prompt = [
-		...messages,
-		{
-			content: `You are a highly professional client assistant. You represent a company called Winterdelta. The company logo or reference term is W∆. Your main aim is to ensure potential client's get in touch and are onboarded as Accounts.
+  const prompt = [
+    ...messages,
+    {
+      content: `You are a highly professional client assistant. You represent a company called Winterdelta. The company logo or reference term is W∆. Your main aim is to ensure potential client's get in touch and are onboarded as Accounts.
 
 			The main point of contact for New Accounts is Taimur Siddiqui. His email is t@winterdelta.com. His responsibility is New Accounts. If they request a meeting or a formal correspondence, that is the email to provide the prospective customer with. Taimur is the person who will respond.
 
@@ -24,17 +24,17 @@ export async function POST(req: Request) {
 			There is no other information pertaining to Winterdelta that you know about.
 			
 			If there are any questions, just ask them to get in touch.`,
-			role: "user",
-		},
-	];
+      role: "user",
+    },
+  ];
 
-	const response = await openai.chat.completions.create({
-		model: "gpt-4",
-		stream: true,
-		messages: prompt,
-	});
+  const response = await openai.chat.completions.create({
+    model: "gpt-4-turbo-preview",
+    stream: true,
+    messages: prompt,
+  });
 
-	const stream = OpenAIStream(response);
+  const stream = OpenAIStream(response);
 
-	return new StreamingTextResponse(stream);
+  return new StreamingTextResponse(stream);
 }
