0b292bb343eadb30086ea019440b2179edc7faeb
Winterdelta <w@winterdelta.com>
2025-04-09 15:52:13 +0100

    copy API

--- complication/Monaco/Lead-Header/Copy.tsx
+++ complication/Monaco/Lead-Header/Copy.tsx
@@ -1,10 +1,15 @@
 "use client";
 
-import { useChat } from "ai/react";
-import { useEffect, useCallback } from "react";
+import { useEffect, useState, useCallback } from "react";
 import styles from "./Copy.module.css";
-import { ChatRequestOptions } from "ai";
 import { IBM_Plex_Serif, IBM_Plex_Sans } from "next/font/google";
+import { useChat } from "@ai-sdk/react";
+
+const serif_font = IBM_Plex_Serif({
+  subsets: ["latin"],
+  style: ["italic", "normal"],
+  weight: ["200", "400"],
+});
 
 const mono = IBM_Plex_Serif({
   subsets: ["latin"],
@@ -18,11 +23,66 @@ const sans = IBM_Plex_Sans({
   weight: ["500"],
 });
 
-const fetcher = (url: RequestInfo | URL) => fetch(url).then((r) => r.json());
+export default function Copy() {
+  const [title, setTitle] = useState<string | null>(null);
+  const { messages, setMessages, append } = useChat({
+    api: "/api/lead-header",
+  });
 
-export default function AI() {
-  return <></>;
+  useEffect(() => {
+    setMessages([]);
+    append({
+      role: "system",
+      content: "",
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    const allMessagesContent_title = messages.map((m) => m.content).join("\n");
+    setTitle(allMessagesContent_title);
+  }, [messages]);
+
+  return (
+    <div className={styles.title}>
+      <div className={`${styles.mai} ${sans.className}`}>{title}</div>
+    </div>
+  );
 }
+
+// return (
+// <div className={styles.ai}>
+//       <div>
+//         {messages.map((m) => (
+//           <div className={styles.mai} key={m.id}>
+//             <div className={sans.className}>
+//               <span>{m.content}</span>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//     <div className={styles.ai}>
+//       <input
+//         value={input}
+//         onChange={(event) => {
+//           setInput(event.target.value);
+//         }}
+//         onKeyDown={async (event) => {
+//           if (event.key === "Enter") {
+//             append({ content: input, role: "user" });
+//           }
+//         }}
+//       />
+
+//       {messages.map((m) => (
+//         <div className={`${styles.mai} ${sans.className}`} key={m.id}>
+//           {m.content}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
 
 // export default function AI() {
 //   const { messages, append } = useChat({
