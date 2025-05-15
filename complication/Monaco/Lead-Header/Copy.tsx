676ec7abb148c4020838d2d4e8bc7d3fbfb93cd9
Winterdelta <w@winterdelta.com>
2025-05-15 20:49:36 +0100

    fix breakpoint bug 🐞 on widescreen

--- complication/Monaco/Lead-Header/Copy.tsx
+++ complication/Monaco/Lead-Header/Copy.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Copy.module.css";
 import { IBM_Plex_Serif, IBM_Plex_Sans } from "next/font/google";
 import { useChat } from "@ai-sdk/react";
@@ -43,91 +43,5 @@ export default function Copy() {
     setTitle(allMessagesContent_title);
   }, [messages]);
 
-  return (
-    <div className={styles.title}>
-      <div className={`${styles.mai} ${sans.className}`}>{title}</div>
-    </div>
-  );
+  return <div className={styles.title}>{title}</div>;
 }
-
-// return (
-// <div className={styles.ai}>
-//       <div>
-//         {messages.map((m) => (
-//           <div className={styles.mai} key={m.id}>
-//             <div className={sans.className}>
-//               <span>{m.content}</span>
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//     <div className={styles.ai}>
-//       <input
-//         value={input}
-//         onChange={(event) => {
-//           setInput(event.target.value);
-//         }}
-//         onKeyDown={async (event) => {
-//           if (event.key === "Enter") {
-//             append({ content: input, role: "user" });
-//           }
-//         }}
-//       />
-
-//       {messages.map((m) => (
-//         <div className={`${styles.mai} ${sans.className}`} key={m.id}>
-//           {m.content}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default function AI() {
-//   const { messages, append } = useChat({
-//     api: "/api/lead-header",
-//   });
-
-//   const handleSubmit = useCallback(
-//     (
-//       // e: React.FormEvent<HTMLFormElement>,
-//       { options, functions, function_call }: ChatRequestOptions = {},
-//       metadata?: Object
-//     ) => {
-//       // e.preventDefault();
-//       append(
-//         {
-//           content: "Respond: ",
-//           role: "user",
-//           createdAt: new Date(),
-//         },
-//         { options, functions, function_call }
-//       );
-//     },
-//     [append]
-//   );
-
-//   useEffect(() => {
-//     append({
-//       role: "system",
-//       content: "",
-//     });
-
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, []);
-
-//   return (
-//     <div className={styles.ai}>
-//       <div>
-//         {messages.map((m) => (
-//           <div className={styles.mai} key={m.id}>
-//             <div className={sans.className}>
-//               <span>{m.content}</span>
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
